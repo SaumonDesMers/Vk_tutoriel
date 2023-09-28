9e3c68e49f75566ff57330a83365bcd4b4c9410b
Simon Gaubert <sim.gaubert.sg@gmail.com>
2023-09-28 16:34:23 +0200

    minor comment update

--- include/application.hpp
+++ include/application.hpp
@@ -19,6 +19,12 @@
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
+#ifdef NDEBUG
+	const bool enableValidationLayers = false;
+#else
+	const bool enableValidationLayers = true;
+#endif
+
 /* List of validation layers to use */
 const std::vector<const char*> validationLayers = {
 	"VK_LAYER_KHRONOS_validation"
@@ -28,12 +34,6 @@ const std::vector<const char*> validationLayers = {
 const std::vector<const char*> deviceExtensions = {
 	VK_KHR_SWAPCHAIN_EXTENSION_NAME /* Swapchain requirement, should be available if presentation queue is available */
 };
-
-#ifdef NDEBUG
-	const bool enableValidationLayers = false;
-#else
-	const bool enableValidationLayers = true;
-#endif
 
 struct QueueFamilyIndices {
 	std::optional<uint32_t> graphicsFamily;
@@ -118,7 +118,7 @@ private:
 
 	/* instance.cpp */
 	void createInstance();
-	std::vector<const char*> getRequiredExtensions();
+	std::vector<const char*> getGlfwRequiredExtensions();
 	bool checkValidationLayerSupport();
 
 	/* debug.cpp */
--- src/application/instance.cpp
+++ src/application/instance.cpp
@@ -1,15 +1,19 @@
 #include "application.hpp"
 
+/*
+ * The instance is the connection between the application and the Vulkan library
+ */
+
 void Application::createInstance() {
 	/* Check if validation layers are available */
 	if (enableValidationLayers && !this->checkValidationLayerSupport()) {
 		throw std::runtime_error("validation layers requested, but not available!");
 	}
 
-	/* Give information about the application to the driver */
+	/* Application info is optional, but may provide some useful information to the driver to optimize for our application */
 	VkApplicationInfo appInfo{};
 	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	appInfo.pApplicationName = "Hello Triangle";
+	appInfo.pApplicationName = "Vulkan Tutorial";
 	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 	appInfo.pEngineName = "No Engine";
 	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
@@ -20,18 +24,18 @@ void Application::createInstance() {
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	createInfo.pApplicationInfo = &appInfo;
 
-	/* Get required extensions and add them to the createInfo */
-	auto extensions = this->getRequiredExtensions();
+	/* Get extensions required by GLFW */
+	auto extensions = this->getGlfwRequiredExtensions();
 	createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
 	createInfo.ppEnabledExtensionNames = extensions.data();
 
 	VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
 	if (enableValidationLayers) {
-		/* Add validation layers to the createInfo */
+		/* Add validation layers */
 		createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
 		createInfo.ppEnabledLayerNames = validationLayers.data();
 
-		/* Add debug messenger to the createInfo so that it is called during instance creation and destruction */
+		/* Add debug messenger so that it is called during instance creation and destruction */
 		this->populateDebugMessengerCreateInfo(debugCreateInfo);
 		createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debugCreateInfo;
 	} else {
@@ -72,7 +76,10 @@ bool Application::checkValidationLayerSupport() {
 	return true;
 }
 
-std::vector<const char*> Application::getRequiredExtensions() {
+/*
+ * Get the list of required extensions for GLFW
+ */
+std::vector<const char*> Application::getGlfwRequiredExtensions() {
 	uint32_t glfwExtensionCount = 0;
 	const char** glfwExtensions;
 	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
