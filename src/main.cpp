75f1a8da1e1fc3a63752b42969eebf617f05cee3
Simon Gaubert <sim.gaubert.sg@gmail.com>
2023-10-07 00:54:42 +0200

    refactor: restore main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,14 @@
 
 int main() {
 
-	// Application app;
+	Application app;
 
-	// try {
-	// 	app.run();
-	// } catch (const std::exception& e) {
-	// 	std::cerr << e.what() << std::endl;
-	// 	return EXIT_FAILURE;
-	// }
+	try {
+		app.run();
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
 
 	return EXIT_SUCCESS;
 }
