5193cf3306a91d70883f87da91d48f1d40edcfd3
Simon Gaubert <sim.gaubert.sg@gmail.com>
2023-11-24 13:40:39 +0100

    add index buffer

--- include/app/model.hpp
+++ include/app/model.hpp
@@ -21,8 +21,14 @@ namespace ft
 			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
 		};
+
+		struct Builder
+		{
+			std::vector<Vertex> vertices{};
+			std::vector<uint32_t> indices{};
+		};
 		
-		Model(Device &device, const std::vector<Vertex> &vertices);
+		Model(Device &device, const Model::Builder &builder);
 		~Model();
 		
 		Model(const Model &) = delete;
@@ -39,7 +45,13 @@ namespace ft
 		VkDeviceMemory m_vertexBufferMemory;
 		uint32_t m_vertexCount;
 
+		bool m_hasIndexBuffer = false;
+		VkBuffer m_indexBuffer;
+		VkDeviceMemory m_indexBufferMemory;
+		uint32_t m_indexCount;
+
 		void createVertexBuffer(const std::vector<Vertex> &vertices);
+		void createIndexBuffer(const std::vector<uint32_t> &indices);
 
 	};
 
