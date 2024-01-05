0505875d9fa6930e720e9c3049c4f9d18a375ac4
Simon <sim.gaubert.sg@gmail.com>
2024-01-05 14:26:53 +0100

    add: command buffer class

---
+++ cppVulkanAPI/src/command_buffer.cpp
@@ -0,0 +1,33 @@
+#include "command_buffer.hpp"
+
+#include <stdexcept>
+
+namespace LIB_NAMESPACE
+{
+	CommandBuffer::CommandBuffer(VkDevice device, const AllocateInfo& allocateInfo)
+		: m_device(device), m_commandPool(allocateInfo.commandPool)
+	{
+		if (vkAllocateCommandBuffers(device, &allocateInfo, &m_commandBuffer) != VK_SUCCESS) {
+			throw std::runtime_error("failed to allocate command buffers.");
+		}
+	}
+
+	CommandBuffer::~CommandBuffer()
+	{
+		vkFreeCommandBuffers(m_device, m_commandPool, 1, &m_commandBuffer);
+	}
+
+	void CommandBuffer::begin(const BeginInfo& beginInfo)
+	{
+		if (vkBeginCommandBuffer(m_commandBuffer, &beginInfo) != VK_SUCCESS) {
+			throw std::runtime_error("failed to begin recording command buffer.");
+		}
+	}
+
+	void CommandBuffer::end()
+	{
+		if (vkEndCommandBuffer(m_commandBuffer) != VK_SUCCESS) {
+			throw std::runtime_error("failed to record command buffer.");
+		}
+	}
+}
---
+++ cppVulkanAPI/src/command_buffer.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "defines.hpp"
+
+#include <vulkan/vulkan.h>
+
+namespace LIB_NAMESPACE
+{
+	class CommandBuffer
+	{
+		public:
+
+		struct AllocateInfo: public VkCommandBufferAllocateInfo
+		{
+			AllocateInfo(): VkCommandBufferAllocateInfo()
+			{
+				this->sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+			}
+		};
+
+		struct BeginInfo: public VkCommandBufferBeginInfo
+		{
+			BeginInfo(): VkCommandBufferBeginInfo()
+			{
+				this->sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+			}
+		};
+
+		CommandBuffer(VkDevice device, const AllocateInfo& allocateInfo);
+		~CommandBuffer();
+
+		VkCommandBuffer getVk() const { return m_commandBuffer; }
+
+		void begin(const BeginInfo& beginInfo);
+		void end();
+	
+	private:
+	
+		VkCommandBuffer m_commandBuffer;
+
+		VkDevice m_device;
+		VkCommandPool m_commandPool;
+
+	};
+}
